b8216232ce90b6a4a807778776b568cd2d867868
chetannanji <chetannanji@gmail.com>
2026-08-18 20:23:17 +0300

    Add files via upload

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+/**
+ * Initialize Express & Prisma
+ */
+export declare const prisma: PrismaClient<{
+    datasources: {
+        db: {
+            url: string;
+        };
+    };
+}, never, import("@prisma/client/runtime/library").DefaultArgs>;
+//# sourceMappingURL=index.d.ts.map
